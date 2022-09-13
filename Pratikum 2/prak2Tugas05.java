5bb7343a4b4c9c0e72aa0abe3b17d42e309aa258
divaaanp <93565057+divaaanp@users.noreply.github.com>
2022-09-13 21:59:14 +0700

    Tugas nomor 5 praktikum 2 selesai

---
+++ Pratikum 2/prak2Tugas05.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class prak2Tugas05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x ;
+        int i ;
+        
+        System.out.println(" ");
+        for (i=0;i<11;i++){
+            if(i%3==0){
+                System.out.print("");
+
+            }else{
+                x=2*i;
+                System.out.print(" "+x);
+
+            }
+        }
+
+    }
+    }
